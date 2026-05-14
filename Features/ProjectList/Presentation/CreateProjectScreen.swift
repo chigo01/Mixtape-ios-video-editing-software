93947891dec07a465b58054242398111de039273
chigo01 <baruch.favour@redbiller.email>
2026-05-14 18:57:54 +0100

    worked on the media selector page/feature

---
+++ Features/ProjectList/Presentation/CreateProjectScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CreateProjectScreen.swift
+//  Mixtape
+//
+//  Created by Favour Baruch on 08/05/2026.
+//
+
+import SwiftUI
+
+struct CreateProjectScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateProjectScreen()
+}
